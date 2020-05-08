6a7d85ac790cc8bfe1177148bff79ece9b49f9e5
Hailton <hailton.martins@gmail.com>
2020-05-08 13:20:44 -0300

    adicionado metodos de lista

--- index.php
+++ index.php
@@ -2,12 +2,28 @@
 
 require_once ("config.php");
 
+//carrega um usuario
+//$root = new Usuario();
+//$root->loadbyid(2);
+//echo $root;
 
-$root = new Usuario();
-$root->loadbyid(2);
+//carrega uma lista de usuarios
 
+//$lista = Usuario::getList();
 
+//echo json_encode($lista);
 
-echo $root;
+//carrega uma lista de usuarios buscando pelo login
+
+//$search = Usuario::search("h");
+
+//echo json_encode($search);
+
+//carrega o usuario pelo login e senha
+
+$usuario = new Usuario();
+$usuario->login("Heloisa","3456");
+
+echo ($usuario);
 
  ?>
